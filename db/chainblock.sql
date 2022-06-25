b16a279cfed7e0d2e9cc88053005292899392282
Your <you@gmail.com>
2022-06-26 00:38:20 +0400

    fixed

--- db/chainblock.sql
+++ db/chainblock.sql
@@ -9,7 +9,7 @@ use cb;
 
 create table t_user(
   id int primary key auto_increment,
-  name varchar(50) comment '用户名',
+  username varchar(50) comment '用户名',
   pwd varchar(100) comment '密码',
   money decimal(10,4) comment '金额',
   salt varchar(100) comment '盐',
@@ -17,15 +17,23 @@ create table t_user(
   create_time timestamp comment '创建时间'
 );
 
+
+insert into cb.t_user (`username`,`pwd`,`money`,`salt`,`version`,`create_time`)
+values
+('jack','123456',10000,'123456',1,'2022-06-25 12:00:00');
+
+
+
 create table t_recharge (
     id int primary key auto_increment,
     transaction_id varchar(100) comment '转帐id',
     token_symbol varchar(20) comment '币种',
     token_decimals int comment '位数',
+    token_address varchar(100) comment '合约地址',
     token_name varchar(20) comment '币种名',
-    block_timestamp timestamp comment '块时间戳',
-    from varchar(100) comment '转帐地址',
-    to varchar(100) comment '收款地址',
+    block_timestamp bigint comment '块时间戳',
+    from_address varchar(100) comment '转帐地址',
+    to_address varchar(100) comment '收款地址',
     type varchar(20) comment '转帐类型',
     value decimal(20,6) comment '转帐金额',
     UNIQUE KEY unique_transaction_id (transaction_id)
@@ -36,9 +44,14 @@ create table t_recharge (
 create table t_wallet(
     id int primary key auto_increment,
     address varchar(100),
-    username varchar(50),
-    type int(10)
+    uid int comment '会员id',
+    username varchar(50) comment '会员名',
+    type int(10) comment '钱包类型'
 ) comment '钱包';
+
+insert into cb.t_wallet (`address`,`uid`,`username`,`type`)
+values
+('TEuyVZdSXR8PaFmB8wX1LiZ3getos5Yuwe',1,'jack', 1);
 
 
 
