4a6eb0fcb2703fa8cdcf9db372d684e91b1e22b5
Your <you@gmail.com>
2022-06-27 23:57:43 +0400

    fixed

--- dapp_api/config/config.js
+++ dapp_api/config/config.js
@@ -1,15 +1,27 @@
 
-
 module.exports = {
 
+  //nile(测试网络)
   dev: {
     //open_from TSpbRADBCTZCNNTwqzoHVQoRHyuRYiyu5y
+    network    : 'nile',
+    fullNode   : 'https://api.nileex.io/',
+    solidityNode   : 'https://api.nileex.io',
+    eventServer   : 'https://event.nileex.io/',
     fromAddress: 'TSpbRADBCTZCNNTwqzoHVQoRHyuRYiyu5y',
     toAddress  : 'TPYXWcPZ9DC9R4PvQniTPSjUegUyBD3kJJ',
     privateKey : '0d35dba8af935d575924cd0d3afd2479248de12aa0f13f547e2e9debcdd025c5'
   },
+
+  //主网
   prod: {
-    privateKey : 'f58c1b3a3db8c4024d34427543dfcd6482b0bc7a0619a7d344b216a3be4f7703'
+    network    : 'main',
+    fullNode   : 'https://api.trongrid.io/',
+    solidityNode   : 'https://api.trongrid.io',
+    eventServer   : 'https://event.trongrid.io/',
+    fromAddress: 'TSpbRADBCTZCNNTwqzoHVQoRHyuRYiyu5y',
+    toAddress  : 'TPYXWcPZ9DC9R4PvQniTPSjUegUyBD3kJJ',
+    privateKey : '0d35dba8af935d575924cd0d3afd2479248de12aa0f13f547e2e9debcdd025c5'
   }
 
 }
